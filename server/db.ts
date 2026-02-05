312c02e99d96ee743881cbfb09175abd6bc91dde
Nitesh Gurjar <niteshgurjar9399@gmail.com>
2026-02-05 19:52:53 +0530

    Force remove prisma config and re-trigger build

--- server/db.ts
+++ server/db.ts
@@ -3,3 +3,5 @@ import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
 });
+
+// Rebuild trigger: Ensure library engine is used
