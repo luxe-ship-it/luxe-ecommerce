f166d1ece345958e7819b8915918aa5228d86150
Nitesh Gurjar <niteshgurjar9399@gmail.com>
2026-02-05 18:46:47 +0530

    Fix vite config typing

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-// https://vitejs.dev/config/
-// https://vitejs.dev/config/
+
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
-  const plugins = [react()];
+  const plugins: PluginOption[] = [react()];
 
   if (mode === 'development') {
     try {
